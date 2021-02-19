d10fc6760b5b4e381af284f55fb25d5211e70770
Daniel Picken <dpicken@users.noreply.github.com>
2021-02-18 19:32:00 -0800

    The timer library should use unsigned long for its time points and durations
    This fixes timer bugs after 65 seconds of uptime.

--- src/timer/timer.c
+++ src/timer/timer.c
@@ -5,18 +5,19 @@
 #define MAX_TIMER_COUNT 8
 
 typedef struct {
-  unsigned int time_point_ms;
+  unsigned long time_point_ms;
   timer_callback callback;
-  unsigned int next_ttl_ms;
+  unsigned long next_ttl_ms;
   unsigned char is_special;
+  unsigned char pad;
 } timer_entry;
 
 timer_entry timers[MAX_TIMER_COUNT];
 
-void timer_add(timer_callback callback, unsigned int ttl_ms, unsigned int next_ttl_ms, unsigned char is_special) {
+void timer_add(timer_callback callback, unsigned long ttl_ms, unsigned long next_ttl_ms, unsigned char is_special) {
   timer_entry* entry;
 
-  int i = 0;
+  unsigned char i = 0;
 
   for (; i != MAX_TIMER_COUNT; ++i) {
     entry = &timers[i];
@@ -33,22 +34,22 @@ void timer_add(timer_callback callback, unsigned int ttl_ms, unsigned int next_t
   // TODO: brk
 }
 
-void timer_add_one_shot(timer_callback callback, unsigned int ttl_ms) {
+void timer_add_one_shot(timer_callback callback, unsigned long ttl_ms) {
   timer_add(callback, ttl_ms, 0, 0);
 }
 
-void timer_add_fixed_rate(timer_callback callback, unsigned int ttl_ms) {
+void timer_add_fixed_rate(timer_callback callback, unsigned long ttl_ms) {
   timer_add(callback, ttl_ms, ttl_ms, 0);
 }
 
-void timer_add_fixed_rate_special(timer_callback callback, unsigned int ttl_ms) {
+void timer_add_fixed_rate_special(timer_callback callback, unsigned long ttl_ms) {
   timer_add(callback, ttl_ms, ttl_ms, 1);
 }
 
 void timer_clear_all_non_special(void) {
   timer_entry* entry;
 
-  int i = 0;
+  unsigned char i = 0;
 
   for (; i != MAX_TIMER_COUNT; ++i) {
     entry = &timers[i];
@@ -61,7 +62,7 @@ void timer_clear_all_non_special(void) {
 void timer_on_system_time_ticked(void) {
   timer_entry* entry;
 
-  int i = 0;
+  unsigned char i = 0;
   unsigned long time_point_ms = system_time_get_ms();
 
   for (; i != MAX_TIMER_COUNT; ++i) {
--- src/timer/timer.h
+++ src/timer/timer.h
@@ -4,13 +4,13 @@
 typedef void (*timer_callback)(void);
 
 /** Add a timer that fires once. */
-void timer_add_one_shot(timer_callback callback, unsigned int ttl_ms);
+void timer_add_one_shot(timer_callback callback, unsigned long ttl_ms);
 
 /** Add a timer that fires at a fixed rate. */
-void timer_add_fixed_rate(timer_callback callback, unsigned int ttl_ms);
+void timer_add_fixed_rate(timer_callback callback, unsigned long ttl_ms);
 
 /** Add a timer that fires at a fixed rate, and, cannot be cleared. */
-void timer_add_fixed_rate_special(timer_callback callback, unsigned int ttl_ms);
+void timer_add_fixed_rate_special(timer_callback callback, unsigned long ttl_ms);
 
 /** Clear all non-special timers. */
 void timer_clear_all_non_special(void);
